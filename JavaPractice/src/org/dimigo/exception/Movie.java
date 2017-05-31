6eeac48a73e7a9ea1313da83b6ed671d475a1cb9
15기 신기섭 <15 @DESKTOP-UOJJJCH>
2017-05-31 10:32:53 +0900

    실습과제 14. 디미 씨네마

---
+++ JavaPractice/src/org/dimigo/exception/Movie.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package org.dimigo.exception;
+
+/**
+ * <pre>
+ * org.dimigo.exception
+ * 	    └ Movie
+ * 1. 개요 :
+ * 2. 작성일: 2017. 5. 31.
+ * </pre>
+ * @author 15기 신기섭
+ * @version : 1.0
+ */
+public class Movie {
+	private String title;
+	private int limitAge;
+	
+	public Movie(String title, int limitAge) {
+		this.title = title;
+		this.limitAge = limitAge;
+	}
+	public String getTitle() { return title; }
+	public int getLimitAge() { return limitAge;}
+	public void buyTicket(int age) throws Exception {
+		try {
+			if( limitAge > age ) throw new Exception(title + "은/는 " + limitAge + "세 이상 관람가입니다.");
+			else System.out.println(title + " 즐감하세요.");
+		} catch (Exception e) {
+			throw e;
+		}
+	}
+
+}
